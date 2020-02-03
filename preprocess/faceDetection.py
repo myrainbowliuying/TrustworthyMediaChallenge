defdc9fb3d6ed8a9d48c41ba6bad735d8bb3652e
rainbow <myrainbowliuying@163.com>
2020-02-03 18:40:16 +0800

    output video with audio

--- preprocess/faceDetection.py
+++ preprocess/faceDetection.py
@@ -1,10 +1,11 @@
 import face_recognition
-import imageio
-import cv2
-import os
+from moviepy.editor import *
 
 # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+
 
 face_directory_name = 'C:\\Users\\research\\rainbow\\fake media\\face_pictures'
 video_directory_name = 'C:\\Users\\research\\rainbow\\fake media\\Video-frame-rate_24'
@@ -14,79 +15,75 @@ f = sorted(f)
 
 for picture_name in f:
     target_face = face_recognition.load_image_file(face_directory_name + '/' + picture_name)
-    target_face_encoding = face_recognition.face_encodings(target_face)[0]
-
-    # input video
-    video_name = picture_name[:-4] + '.mp4'
-    reader = imageio.get_reader(video_directory_name + '/' + video_name, 'ffmpeg')
-    fps = reader.get_meta_data()['fps']
-
-    read_size = cv2.VideoCapture(video_directory_name + '/' + video_name)
-    width = int(read_size.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(read_size.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    read_size.release()
-    print(width, height)
-
-    output_name = picture_name[:-4] + '_1080.mp4'
-    writer = imageio.get_writer(output_directory_name + '/' + output_name, fps=fps)
-
+    target_face_encoding = face_recognition.face_encodings(target_face)
+    if len(target_face_encoding)==0:
+        print('failed recognize target image'+picture_name)
+    # some temporary variables.
     golden_ratio = 0.618
-    frames = []
     delete_flag = 0
     size_flag = 0
-
     before_flag = 0  # 0 means not delete
     real_delete_flag = 0
+    output_video_list=[]
+    # input video
+    video_name = picture_name[:-4] + '.mp4'
+    print('begin: '+video_name)
 
-    for num, im in enumerate(reader):
+    video = VideoFileClip(video_directory_name + '/' + video_name)
 
-        frames.append(im)
+    #output video
+    output_video_name = picture_name[:-4] + '_1080.mp4'
 
-        if (num % 17) == 0:
-            unknown_face_encodings = face_recognition.face_encodings(im)
-            face_locations = face_recognition.face_locations(im)
+    #begin detect
+    width,height=video.size
+    duration = video.duration  # == audio.duration, presented in seconds, float
+    step = 0.5
+    for t in range(1,int(duration / step)):  # runs through audio/video frames obtaining them by timestamp with step 100 msec
+        t = t * step
+        if t > video.duration: break
+        video_frame = video.get_frame(t)  # numpy array representing RGB/gray frame
+        unknown_face_encodings = face_recognition.face_encodings(video_frame)
+        unknown_face_encodings = face_recognition.face_encodings(video_frame)
+        face_locations = face_recognition.face_locations(video_frame)
 
-            if len(unknown_face_encodings) == 0:
-                size_flag = 1
-                delete_flag = 1
-            else:
-                i = -1
-                for unknown_face_encoding in unknown_face_encodings:
-                    i = i + 1
-                    results = face_recognition.compare_faces([target_face_encoding], unknown_face_encoding)
-                    if results[0] == False:
-                        delete_flag = 1
-                    else:
-                        delete_flag = 0
-                        break
-
-                face_location = face_locations[i]
-                top, right, bottom, left = face_location
-                center = ((bottom + top) / 2, (right + left) / 2)
-                print(face_location)
-                if ((bottom - top) * (right - left) / width / height > 0.25) or center[0] < height * (
-                        0.5 - golden_ratio / 2) or \
-                        center[0] > height * (0.5 + golden_ratio / 2) or center[1] > width * (0.5 + golden_ratio / 2) or \
-                        center[
-                            1] < width * (0.5 - golden_ratio / 2):
-                    size_flag = 1
+        if len(unknown_face_encodings) == 0:
+            size_flag = 1
+            delete_flag = 1
+        else:
+            i = -1
+            for unknown_face_encoding in unknown_face_encodings:
+                i = i + 1
+                result = face_recognition.compare_faces([target_face_encoding], unknown_face_encoding)
+                if result == False:
+                    delete_flag = 1
                 else:
-                    size_flag = 0
+                    delete_flag = 0
+                    break
 
-            if before_flag == 1 and size_flag == 0 and delete_flag == 0:
-                print("before flag = 0")
-                before_flag=0
-                frames.clear()
-            elif size_flag == 1 or delete_flag == 1:
-                print("before flag = 1")
-                before_flag = 1
-                frames.clear()
+            face_location = face_locations[i]
+            top, right, bottom, left = face_location
+            center = ((bottom + top) / 2, (right + left) / 2)
+            print(face_location)
+            if ((bottom - top) * (right - left) / width / height > 0.25) or center[0] < height * (
+                    0.5 - golden_ratio / 2) or \
+                    center[0] > height * (0.5 + golden_ratio / 2) or center[1] > width * (0.5 + golden_ratio / 2) or \
+                    center[
+                        1] < width * (0.5 - golden_ratio / 2):
+                size_flag = 1
             else:
-                before_flag = 0
-                print("add")
-                for frame in frames:
-                    writer.append_data(frame)
-                frames.clear()
+                size_flag = 0
 
-    print(video_name + 'finish')
-    writer.close()
+        if before_flag == 1 and size_flag == 0 and delete_flag == 0:
+            print("before flag = 0")
+            before_flag = 0
+        elif size_flag == 1 or delete_flag == 1:
+            print("before flag = 1")
+            before_flag = 1
+        else:
+            before_flag = 0
+            print("add")
+            output_video_list.append(video.subclip(t-step,t))
+
+    print(video_name + ' finished')
+    output_video=concatenate_videoclips(output_video_list)
+    output_video.write_videofile(output_directory_name+'/'+output_video_name)
